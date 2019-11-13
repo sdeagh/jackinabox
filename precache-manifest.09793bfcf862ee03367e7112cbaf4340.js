22ec71a37832bb7aa25ec2b6e621f3b7fc8f40e4
sdeagh <huw.burton@btinternet.com>
2019-11-13 10:23:54 +0000

    Updates

---
+++ precache-manifest.09793bfcf862ee03367e7112cbaf4340.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01131e0413518d9dd660411428c9e993",
+    "url": "/jackinabox/index.html"
+  },
+  {
+    "revision": "200eccd4f420f91774b7",
+    "url": "/jackinabox/static/js/2.6a1b8c66.chunk.js"
+  },
+  {
+    "revision": "e8282786c99521552b45",
+    "url": "/jackinabox/static/js/main.d60883f3.chunk.js"
+  },
+  {
+    "revision": "6486c57320402fa4aa2a",
+    "url": "/jackinabox/static/js/runtime-main.f5b999df.js"
+  },
+  {
+    "revision": "56c7c9fdecf5bbd9dc8b8b6563171fa2",
+    "url": "/jackinabox/static/media/box1.56c7c9fd.jpeg"
+  }
+]);
