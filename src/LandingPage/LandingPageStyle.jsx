8a7c3e0565e28f31f107b85aa1d3df22b4ff43c9
sdeagh <huw.burton@btinternet.com>
2020-01-19 18:47:03 +0000

    box shadow update

--- src/LandingPage/LandingPageStyle.jsx
+++ src/LandingPage/LandingPageStyle.jsx
@@ -21,8 +21,8 @@ const landingPageStyle = (theme) => ({
     zIndex: '3',
     margin: '0 30px 0px 30px',
     borderRadius: theme.spacing(1),
-    boxShadow:
-      '0 16px 24px 2px rgba(0, 0, 0, 0.14), 0 6px 30px 5px rgba(0, 0, 0, 0.12), 0 8px 10px -5px rgba(0, 0, 0, 0.2)',
+    // boxShadow: '4px 4px 24px 11px rgba(255,255,255,0.19)',
+    boxShadow: '6px 6px 24px 2px rgba(255, 255, 255, 0.14), 6px 6px 24px 5px rgba(255, 255, 255, 0.12), 0px 6px 1px -5px rgba(255, 255, 255, 0.12)',
   },
   mainRaised: {
     margin: '-60px 30px 0px',
