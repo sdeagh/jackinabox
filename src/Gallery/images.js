8369e9b5532225310dbef42de7602767481e26be
Huw Burton <huw.burton@btiinternet.com>
2020-03-14 20:20:50 +0000

    adding images

--- src/Gallery/images.js
+++ src/Gallery/images.js
@@ -1,7 +1,7 @@
 const images = [
   {
-    original: 'https://picsum.photos/id/1018/1000/600/',
-    thumbnail: 'https://picsum.photos/id/1018/250/150/',
+    original: './img/box1.JPG',
+    thumbnail: './img/box1.JPG',
   },
   {
     original: 'https://picsum.photos/id/1015/1000/600/',
