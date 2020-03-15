644766d4c85ddc06a48084ed3337dd3c7414c0f1
Huw Burton <huw.burton@btiinternet.com>
2020-03-15 10:08:57 +0000

    adding pics

--- src/Gallery/images.js
+++ src/Gallery/images.js
@@ -4,12 +4,12 @@ const images = [
     thumbnail: './img/box1.JPG',
   },
   {
-    original: 'https://picsum.photos/id/1015/1000/600/',
-    thumbnail: 'https://picsum.photos/id/1015/250/150/',
+    original: './img/box2.JPG',
+    thumbnail: './img/box2.JPG',
   },
   {
-    original: 'https://picsum.photos/id/1019/1000/600/',
-    thumbnail: 'https://picsum.photos/id/1019/250/150/',
+    original: './img/box3.JPG',
+    thumbnail: './img/box3.JPG',
   },
 ];
 
