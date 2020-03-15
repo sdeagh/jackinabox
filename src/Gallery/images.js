d5dad4556dfb14fcfe2900e77289f5499b8f34da
sdeagh <huw.burton@btinternet.com>
2020-03-15 22:38:25 +0000

    adding photos

--- src/Gallery/images.js
+++ src/Gallery/images.js
@@ -1,15 +1,43 @@
 const images = [
   {
-    original: './img/box1.JPG',
-    thumbnail: './img/box1.JPG',
+    original: './img/box1-1000.JPG',
+    thumbnail: './img/box1-250.JPG',
   },
   {
-    original: './img/box2.JPG',
-    thumbnail: './img/box2.JPG',
+    original: './img/box2-1000.JPG',
+    thumbnail: './img/box2-250.JPG',
   },
   {
-    original: './img/box3.JPG',
-    thumbnail: './img/box3.JPG',
+    original: './img/box3-1000.JPG',
+    thumbnail: './img/box3-250.JPG',
+  },
+  {
+    original: './img/box4-1000.JPG',
+    thumbnail: './img/box4-250.JPG',
+  },
+  {
+    original: './img/box5-1000.JPG',
+    thumbnail: './img/box5-250.JPG',
+  },
+  {
+    original: './img/box6-1000.JPG',
+    thumbnail: './img/box6-250.JPG',
+  },
+  {
+    original: './img/box7-1000.JPG',
+    thumbnail: './img/box7-250.JPG',
+  },
+  {
+    original: './img/box8-1000.JPG',
+    thumbnail: './img/box8-250.JPG',
+  },
+  {
+    original: './img/box9-1000.JPG',
+    thumbnail: './img/box9-250.JPG',
+  },
+  {
+    original: './img/box10-1000.JPG',
+    thumbnail: './img/box10-250.JPG',
   },
 ];
 
